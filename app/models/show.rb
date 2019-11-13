3bf96275e8f30346a23a3d8d119c6f4692d8ed78
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 02:13:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     name = Show.joins(:characters, :actors).pluck("actors.first_name, actors.last_name").first.join(" ")
+    list = name.split
     binding.pry
   end
 end
