169f4263d47e0acf3b141313985c30e576177849
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 02:29:26 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,8 +4,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
+    binding.pry
     name = self.joins(:characters, :actors).pluck("actors.first_name, actors.last_name").first.join(" ")
-    # binding.pry
+
 
   end
 end
