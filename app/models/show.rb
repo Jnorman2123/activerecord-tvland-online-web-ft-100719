d54a1357f0cbd32537968bc2cd7d0c5801a3b854
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 01:53:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,6 +7,6 @@ class Show < ActiveRecord::Base
 
     related_show = Show.joins(:actors, :characters).where(characters: {id: id})
     name_array = related_show.pluck("actors.first_name, actors.last_name")
-    p name_array.first.join(" ")
+    p "#{name_array}.first.join(" ")"
   end
 end
