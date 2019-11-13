69efaefcd917f084e3c1e993c78104c1d49a06af
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 01:17:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   belongs_to :network
   has_many :characters
   has_many :actors, through: :characters
+
+  def actors_list
+    Show.joins(:actors, :characters).pluck("actors.first_name, actors.last_name").first.join(" ")
+  end
 end
