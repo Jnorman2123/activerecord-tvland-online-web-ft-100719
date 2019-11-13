414a7d89550e7b0c4f5331f2585220dc51bc027e
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 01:58:30 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
 
-    related_show = Show.joins(:actors, :characters).where(characters: {id: id})
+    related_show = Show.joins(:actors, :characters).find_by(characters: {id: id})
     # name = related_show.pluck("actors.first_name, actors.last_name").first.join(" ")
   end
 end
