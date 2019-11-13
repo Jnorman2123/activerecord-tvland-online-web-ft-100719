d8a51a7677d9c9096c6fc93f6b464780172f8f05
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 00:53:01 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    self.pluck(:first_name, :last_name)
+    Actor.pluck(:first_name, :last_name)
   end
 end
