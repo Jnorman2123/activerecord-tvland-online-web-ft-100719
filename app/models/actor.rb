ccaef82eac78b2a494bbaddefd1363a2682f141f
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 02:37:58 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    Actor.pluck(:first_name, :last_name).first.join(" ")
+  end
+
+  def list_roles
+    Actor.joins(:characters, :shows).pluck("characters.name, shows.name").first.join(" - ")
+  end
 end
