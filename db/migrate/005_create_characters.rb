e7c7e759a8aa22e28ada0648b605004f6d7cb112
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 00:40:17 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,3 +1,11 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
 
-end 
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+    end
+  end
+
+end
