52f04cd8af7905f6afb035ac9957912f42a759ce
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 02:20:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    name = Show.joins(:characters, :actors).pluck("actors.first_name, actors.last_name").first.join(" ").to_a
+    name = Show.joins(:characters, :actors).pluck("actors.first_name, actors.last_name").first.join("").to_a
   end
 end
